8000abece04941286c9d1b881d5292b4840c5d15
Mobeen <mobeen.bhatti@gmail.com>
2015-04-17 11:12:28 +0500

    Fix design issue reported by Client

--- HaccpBuilder/ControlPanel/Location/LocationMaster.master.cs
+++ HaccpBuilder/ControlPanel/Location/LocationMaster.master.cs
@@ -1,17 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections;
+using System.Configuration;
+using System.Data;
 using System.Linq;
 using System.Web;
+using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using System.Xml.Linq;
 
-namespace HaccpBuilder.ControlPanel.Location
+public partial class ControlPanel_LocationMaster : System.Web.UI.MasterPage
 {
-    public partial class LocationMaster : System.Web.UI.MasterPage
+    protected void Page_Load(object sender, EventArgs e)
     {
-        protected void Page_Load(object sender, EventArgs e)
+        if (Context.Session != null)
         {
-
+            //Tested and the IsNewSession is more advanced then simply checking if 
+            // a cookie is present, it does take into account a session timeout, because 
+            // I tested a timeout and it did show as a new session
+            if (Session.IsNewSession)
+            {
+                // If it says it is a new session, but an existing cookie exists, then it must 
+                // have timed out (can't use the cookie collection because even on first 
+                // request it already contains the cookie (request and response
+                // seem to share the collection)
+                string szCookieHeader = Request.Headers["Cookie"];
+                if ((null != szCookieHeader) && (szCookieHeader.IndexOf("ASP.NET_SessionId") >= 0))
+                {
+                    // Request.Headers["Cookie"] = null;
+                    // Response.Redirect("~/TimeOut.aspx");
+                   // FormsAuthentication.SignOut();
+                    // Response.Redirect("~/Default.aspx", true);
+                    Response.Redirect("~/ControlPanel/Logout.aspx");
+                }
+            }
         }
+        //if (!IsPostBack)
+        //{
+           
+        //}
     }
 }
--- HaccpBuilder/ControlPanel/Location/LocationMaster.master.designer.cs
+++ HaccpBuilder/ControlPanel/Location/LocationMaster.master.designer.cs
@@ -7,45 +7,25 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace HaccpBuilder.ControlPanel.Location {
+
+
+public partial class ControlPanel_LocationMaster {
     
+    /// <summary>
+    /// Head2 control.
+    /// </summary>
+    /// <remarks>
+    /// Auto-generated field.
+    /// To modify move field declaration from designer file to code-behind file.
+    /// </remarks>
+    protected global::System.Web.UI.HtmlControls.HtmlHead Head2;
     
-    public partial class LocationMaster {
-        
-        /// <summary>
-        /// head control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
-        
-        /// <summary>
-        /// form1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-        
-        /// <summary>
-        /// pageTitle control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder pageTitle;
-        
-        /// <summary>
-        /// PlaceHolder control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder PlaceHolder;
-    }
+    /// <summary>
+    /// PlaceHolder control.
+    /// </summary>
+    /// <remarks>
+    /// Auto-generated field.
+    /// To modify move field declaration from designer file to code-behind file.
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.ContentPlaceHolder PlaceHolder;
 }
